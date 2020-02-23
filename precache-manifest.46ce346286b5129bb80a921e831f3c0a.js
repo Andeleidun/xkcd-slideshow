90d97ae3c91f55627c2223af99b2b7fe00d421fd
Adair Daniels <adairdaniels@gmail.com>
2020-02-22 17:00:59 -0800

    Updates

---
+++ precache-manifest.46ce346286b5129bb80a921e831f3c0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170dc0676bc31485d44a035174bb0073",
+    "url": "/xkcd-slideshow/index.html"
+  },
+  {
+    "revision": "a5061dc7321899be199d",
+    "url": "/xkcd-slideshow/static/css/main.6670f2c8.chunk.css"
+  },
+  {
+    "revision": "677a5e7e55fda8a10662",
+    "url": "/xkcd-slideshow/static/js/2.d593be63.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/xkcd-slideshow/static/js/2.d593be63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5061dc7321899be199d",
+    "url": "/xkcd-slideshow/static/js/main.c0ab6721.chunk.js"
+  },
+  {
+    "revision": "62266352991254f311ae",
+    "url": "/xkcd-slideshow/static/js/runtime-main.bbd46f6f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/xkcd-slideshow/static/media/logo.5d5d9eef.svg"
+  }
+]);
